8da086ec1ecdc04627b2e0d56db12a56cc514c81
Rafia Bushra <39@teragrep.com>
2022-01-14 13:59:53 +0200

    Updated License statement

--- src/main/java/com/teragrep/rlp_01/RelpConnection.java
+++ src/main/java/com/teragrep/rlp_01/RelpConnection.java
@@ -1,47 +1,18 @@
 /*
- * Java Reliable Event Logging Protocol Library RLP-01
- * Copyright (C) 2021  Suomen Kanuuna Oy
- *  
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *  
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *  
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *  
- *  
- * Additional permission under GNU Affero General Public License version 3
- * section 7
- *  
- * If you modify this Program, or any covered work, by linking or combining it 
- * with other code, such other code is not for that reason alone subject to any
- * of the requirements of the GNU Affero GPL version 3 as long as this Program
- * is the same Program as licensed from Suomen Kanuuna Oy without any additional
- * modifications.
- *  
- * Supplemented terms under GNU Affero General Public License version 3
- * section 7
- *  
- * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
- * versions must be marked as "Modified version of" The Program.
- *  
- * Names of the licensors and authors may not be used for publicity purposes.
- *  
- * No rights are granted for use of trade names, trademarks, or service marks
- * which are in The Program if any.
- *  
- * Licensee must indemnify licensors and authors for any liability that these
- * contractual assumptions impose on licensors and authors.
- *  
- * To the extent this program is licensed as part of the Commercial versions of
- * Teragrep, the applicable Commercial License may apply to this file if you as
- * a licensee so wish it.
+   Java Reliable Event Logging Protocol Library RLP-01
+   Copyright (C) 2021, 2022  Suomen Kanuuna Oy
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
  */
 
 package com.teragrep.rlp_01;
