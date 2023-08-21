f3766510fb8f0fc6e961a6f63df7a363f587945a
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2023-08-21 16:15:47 +0300

    Modernize existing test, remove unnecessary one

--- src/test/java/com/teragrep/rlp_01/UnitTest.java
+++ src/test/java/com/teragrep/rlp_01/UnitTest.java
@@ -19,87 +19,83 @@ package com.teragrep.rlp_01;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class UnitTest {
 	
-	@Rule
-	public ExpectedException exceptionRule = ExpectedException.none();
-	
 	@Test
-	public void testCode200() throws UnsupportedEncodingException {
+	public void testCode200() {
 		String message = "200 OK";
 		int len = message.length();
 		ByteBuffer buffer = ByteBuffer.allocateDirect(len);
-		buffer.put(message.getBytes("UTF-8"));
+		buffer.put(message.getBytes(StandardCharsets.UTF_8));
 		buffer.flip();
 		RelpFrameRX frame = new RelpFrameRX(2, RelpCommand.SYSLOG, len, buffer);
 		assertEquals(frame.getResponseCode(), 200);
 	}
 	
 	@Test
-	public void testCode500() throws UnsupportedEncodingException {
+	public void testCode500() {
 		String message = "500 NOK";
 		int len = message.length();
 		ByteBuffer buffer = ByteBuffer.allocateDirect(len);
-		buffer.put(message.getBytes("UTF-8"));
+		buffer.put(message.getBytes(StandardCharsets.UTF_8));
 		buffer.flip();
 		RelpFrameRX frame = new RelpFrameRX(2, RelpCommand.SYSLOG, len, buffer);
 		assertEquals(frame.getResponseCode(), 500);
 	}
 	
 	@Test
-	public void testCode123() throws UnsupportedEncodingException {
+	public void testCode123() {
 		String message = "123 OK";
 		int len = message.length();
 		ByteBuffer buffer = ByteBuffer.allocateDirect(len);
-		buffer.put(message.getBytes("UTF-8"));
+		buffer.put(message.getBytes(StandardCharsets.UTF_8));
 		buffer.flip();
 		RelpFrameRX frame = new RelpFrameRX(2, RelpCommand.SYSLOG, len, buffer);
 		assertEquals(frame.getResponseCode(), 123);
 	}
 	
 	@Test
-	public void testCodeEmpty() throws UnsupportedEncodingException {
+	public void testCodeEmpty() {
 		String message = "";
-		int len = message.length();
+		int len = 0;
 		ByteBuffer buffer = ByteBuffer.allocateDirect(len);
-		buffer.put(message.getBytes("UTF-8"));
+		buffer.put(message.getBytes(StandardCharsets.UTF_8));
 		buffer.flip();
-		RelpFrameRX frame = new RelpFrameRX(2, RelpCommand.SYSLOG, len, buffer);
-		exceptionRule.expect(IllegalArgumentException.class);
-		exceptionRule.expectMessage("response code not present");
-		frame.getResponseCode();		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			RelpFrameRX frame = new RelpFrameRX(2, RelpCommand.SYSLOG, len, buffer);
+			frame.getResponseCode();
+		});
 	}
 	
 	@Test
-	public void testCodeNoCode() throws UnsupportedEncodingException {
+	public void testCodeNoCode() {
 		String message = " ";
 		int len = message.length();
 		ByteBuffer buffer = ByteBuffer.allocateDirect(len);
-		buffer.put(message.getBytes("UTF-8"));
+		buffer.put(message.getBytes(StandardCharsets.UTF_8));
 		buffer.flip();
-		RelpFrameRX frame = new RelpFrameRX(2, RelpCommand.SYSLOG, len, buffer);
-		exceptionRule.expect(IllegalArgumentException.class);
-		exceptionRule.expectMessage("response code not a number");
-		frame.getResponseCode();		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			RelpFrameRX frame = new RelpFrameRX(2, RelpCommand.SYSLOG, len, buffer);
+			frame.getResponseCode();
+		});
 	}
 	
 	@Test
-	public void testCode2000() throws UnsupportedEncodingException {
+	public void testCode2000() {
 		String message = "2000 OK";
 		int len = message.length();
 		ByteBuffer buffer = ByteBuffer.allocateDirect(len);
-		buffer.put(message.getBytes("UTF-8"));
+		buffer.put(message.getBytes(StandardCharsets.UTF_8));
 		buffer.flip();
-		RelpFrameRX frame = new RelpFrameRX(2, RelpCommand.SYSLOG, len, buffer);
-		exceptionRule.expect(IllegalArgumentException.class);
-		exceptionRule.expectMessage("response code too long");
-		frame.getResponseCode();		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			RelpFrameRX frame = new RelpFrameRX(2, RelpCommand.SYSLOG, len, buffer);
+			frame.getResponseCode();
+		});
 	}
 }
